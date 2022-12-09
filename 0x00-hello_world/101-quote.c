830e2ecfecb7e54222a4a782b7ecad8194fc635e
francis jumbo <francizoid2all@gmail.com>
2022-12-08 19:58:35 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 2   #include <unistd.h>
 3
 4   /**
-5    * main - print quote *
+5    *main - print quote
 6    *
 7    * Description: prints Dora's quote
 8    *
@@ -11,6 +11,6 @@
 11
 12  int main(void)
 13  {
-14  write(2, "and that piece of art is usesful\" - Dora Korpar, 2015-10-19\n", 59);
+14  write(2,"and that piece of art is usesful\" - Dora Korpar, 2015-10-19\n",59);
 15  return (1);
 16  }
