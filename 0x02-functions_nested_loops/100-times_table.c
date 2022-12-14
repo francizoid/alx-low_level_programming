315d52555c3bc490d72b150a96b932e3fbe9a74d
francis jumbo <francizoid2all@gmail.com>
2022-12-14 12:25:33 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,7 +13,6 @@ void print_times_table(int n)
 {
 	int row, column, product;
 
-
 	if (n >= 0 && n < 15)
 	{
 		for (row = 0; row <= n; row++)
@@ -37,10 +36,10 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar((product / 10) % 10 + '0');
+					_putchar((product / 10) % 10 = '0');
 					_putchar(product % 10 + '0');
 				}
-				else if (product > 99 && product < 1000)
+				else if (product > 99 && product < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
