9a9ef24d03b56adb06c1ea1c3852a369bbe92917
francis jumbo <francizoid2all@gmail.com>
2022-12-16 11:18:32 -0800

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,20 +1,18 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * print_line - prints a line of n long
- * @n: length of line
+ * print_line - prints a line of underscores of a given length
+ *
+ * @n: length of line to print
  *
  * Return: void
  */
-
 void print_line(int n)
 {
-	int line = 0;
-
-	while (line < n)
+	while (n > 0)
 	{
-		_putchar('-');
-		line++;
+		_putchar('_');
+		n--;
 	}
 	_putchar('\n');
 }
