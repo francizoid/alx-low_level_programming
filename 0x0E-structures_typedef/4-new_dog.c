dbf94bb2a46b302dbc863b23af429224cd916650
francis jumbo <francizoid2all@gmail.com>
2023-01-16 03:41:15 -0800

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -79,7 +79,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	doggo->name = _strcopy(doggo->name, name);
 	doggo->age = age;
-	doggo->owner = strcopy(doggo->owner, owner);
+	doggo->owner = _strcopy(doggo->owner, owner);
 
 	return (doggo);
 }
